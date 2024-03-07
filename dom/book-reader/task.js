936c771d50c3ea8bfbddeb712359800746fa2ccb
Nikita-2406 <89524208686@mail.ru>
2024-03-07 21:34:08 +0300

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,14 +3,13 @@ const div = document.getElementsByClassName("book")[0]
 
 for (const elem of a) {
   elem.addEventListener('click', () => {
-    let active = document.getElementsByClassName("font-size_active")
-    for (const act of active) {
-      act.classList.remove("font-size_active")      
-    }
+    let active = document.getElementsByClassName("font-size_active") 
+    active[0].classList.remove("font-size_active")
     elem.classList.add('font-size_active')
 
 
     if (Boolean(elem.getAttribute("data-size"))) {
+      div.classList.remove(div.classList[1])
       div.classList.add("book_fs-" + elem.getAttribute("data-size"))
     } else {
       div.classList.remove(div.classList[1])
